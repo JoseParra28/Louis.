8fde7790aff434566d1a7153def8e8fd284e4bc8
JoseParra28 <iliftandcook@gmail.com>
2023-05-05 22:15:59 +0000

    add product category views

--- store/views.py
+++ store/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 
 
 def store(request):
     return render(request, 'store/store.html')
+
+
+def categories(request):
+    all_categories = Category.objects.all()
+    context = {'all_categories': all_categories}   
+    return render(request, 'store/store.html', context)
+
+
+def pie(request):
+    # request.session.set_expiry(0)
+    pies = Pie.objects.all()
+    ctx = {'pies': pies}
+    print(pies)
+    return render(request, "food/pies.html", ctx)
