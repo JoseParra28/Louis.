c771aaf80c1b303876bce50b99bf2901a74cbddb
JoseParra28 <iliftandcook@gmail.com>
2023-05-06 21:42:15 +0000

    tadd shopping cart functionality

---
+++ store/static/javascript/base.js
@@ -0,0 +1,20 @@
+$(document).on('click', '#add-button', function(e){
+    e.preventDefault();
+
+    $.ajax({
+        type: 'POST',
+        url: '{% url "cart-add" %}',
+        data: {
+            product_id: $('#add-buttton').val(),
+            product_quantity: $('#select option:selected').text(),
+            csrfmiddlewaretoken: "{{ csrf_token }}",
+            action: 'post'
+        },
+        success: function(json){
+
+        },
+        error : function (xhr, errmsg, err){
+
+        },
+    });
+})
