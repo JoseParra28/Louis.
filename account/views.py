28bfa426c94b2e41329ce5ab61f2a46f629f3411
JoseParra28 <iliftandcook@gmail.com>
2023-05-07 18:05:33 +0000

    begin user redistration in views

--- account/views.py
+++ account/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm
+from django.contrib.sites.shortcuts import get_current_site
+from .token import user_tokenizer_generate
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 
 
