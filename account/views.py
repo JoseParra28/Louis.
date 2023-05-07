abb1a1336c7cc3046c05e17d3f924facefaf2841
JoseParra28 <iliftandcook@gmail.com>
2023-05-07 20:21:46 +0000

    created user verification funtinality

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from .forms import CreateUserForm
 from django.contrib.sites.shortcuts import get_current_site
 from .token import user_tokenizer_generate
 from django.template.loader import render_to_string
+from django.contrib.auth.models import User
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
@@ -14,21 +15,50 @@ def register(request):
         form = CreateUserForm(request.POST)
 
         if form.is_valid():
-            form.save()
-            return redirect('store')
+            user = form.save()
+            user.is_active = False
+            user.save()
+            # Email verification setup (template)
+            current_site = get_current_site(request)
+            subject = 'Account verification email'
+            message = render_to_string('account/registration/email-verification.html', {           
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),            
+            })
+            user.email_user(subject=subject, message=message)
 
-    context = {'form': form}
-    return render(request, 'account/registration/register.html', context)
+            return redirect('account/registration/email.verification.sent.html')
+    context = {'form':form}
+    return render(request, 'account/registration/register.html', context=context)
+
+    
 
 
-def email_verification(request):
-    pass
+def email_verification(request, uidb64, token):
+    unique_id = force_text(urlsafe_base64_decode(uidb64))
+    user = User.objects.get(pk=unique_id)    
+    # Success
+    if user and user_tokenizer_generate.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return redirect('email-verification-success')
+         # Failed 
+    else:
+        return redirect('email-verification-failed')
 
+# Verification sent
 def email_verification_sent(request):
-    pass
+    return render(request, 'account/registration/email-verification-sent.html')
 
+# Verification successful
 def email_verification_success(request):
-    pass
+    return render(request, 'account/registration/email-verification-success.html')
 
+
+# Verification failed
 def email_verification_failed(request):
-    pass
+    return render(request, 'account/registration/email-verification-failed.html')
+
+
