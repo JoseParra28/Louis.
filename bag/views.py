2c23971092fd87ab02a05cf513ade4f1c872172e
JoseParra28 <iliftandcook@gmail.com>
2023-05-13 17:37:58 +0000

    added the remove and update funtionality

--- bag/views.py
+++ bag/views.py
@@ -45,35 +45,49 @@ def add_to_bag(request, item_id):
     return redirect(redirect_url)
 
 
-    def adjust_bag(request, item_id):
-        quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+def adjust_bag(request, item_id):
+    quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']       
     bag = request.session.get('bag', {})
 
     if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity              
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
-                
+        if quantity > 0:
+            bag['item_id']['items_by_size'][size] = quantity
         else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
+            del bag['item_id']['items_by_size'][size]
+            if not bag['item_id']['items_by_size']:
+                bag.pop(item_id)     
     else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
+        if quantity > 0:
+            bag['item_id'] = quantity
         else:
-            bag[item_id] = quantity        
+            bag.pop(item_id)    
 
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
-        # bag[item_id] += quantity
         bag[item_id]  = quantity
     request.session['bag'] = bag
 
-    return redirect(redirect_url)
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']       
+    bag = request.session.get('bag', {})
+
+    if size:       
+            del bag['item_id']['items_by_size'][size]
+            if not bag['item_id']['items_by_size']:
+                bag.pop(item_id)     
+    else:
+        bag.pop(item_id)        
+
+    request.session['bag'] = bag
+
+    return HttpResponse(status=200)   
